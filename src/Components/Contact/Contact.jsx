55687b0a0da783049ecf8c3d290fb1f1242e2619
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-04-28 21:36:39 +0300

    Updates

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,27 @@
 const Contact = () => {
     return (
         <div className="contact">
-            <h2>Contact</h2>
+            <div className="contact-text">
+                <h3>Ready to talk?</h3>
+                <h1>Contact Us</h1>
+                <p>
+                    Massa tempor nec feugiat nisl pretium fusce id velit. Accumsan tortor posuere ac ut.
+                    Aenean pharetra magna ac placerat vestibulum lectus. Praesent semper feugiat nibh sed pulvinar.
+                    Lorem sed risus ultricies tristique.
+            </p>
+                <p>
+                    Dictumst vestibulum rhoncus est pellentesque elit ullamcorper.
+                    Amet dictum sit amet justo donec enim diam vulputate. Ut morbi tincidunt augue interdum
+                    velit euismod in pellentesque. Volutpat lacus laoreet non curabitur.
+            </p>
+            </div>
+
+            <div className="contact-form">
+                <input type="text" placeholder="Enter your Name" />
+                <input type="text" placeholder="Enter a valid email adress" />
+                <textarea placeholder="Enter your message" rows="4" cols="50"></textarea>
+                <button>Submit</button>
+            </div>
         </div>
     );
 }
