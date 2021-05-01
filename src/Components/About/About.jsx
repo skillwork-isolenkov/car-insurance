150bb917541d4d8662ae8584268c8f62f929c63b
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-05-01 13:00:55 +0300

    Updates

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,23 @@
+
+
 const About = () => {
     return (
         <div className="about">
-            <h2>About</h2>
+            <iframe title="Car Insurance Video" width="500" height="400" src="https://www.youtube.com/embed/KHe6ZDH5mvY"></iframe>
+            <div className="about-company">
+                <h1>
+                    Know About
+                    <br />
+                    Company
+                </h1>
+                <p>
+                    Donec volutpat enim at interdum pretium. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                    posuere cubilia Curae; Donec sodales mauris pretium scelerisque lobortis. Aenean venenatis suscipit vehicula.
+                    Phasellus condimentum laoreet nibh at consectetur. Suspendisse potenti mauris pretium. Cras sollcitudin lorem risus,
+                    sed ultrices odio aliquam at. Mauris posuere diam at enim malesuada, ac malesuada erat.
+                </p>
+                <button>Learn More</button>
+            </div>
         </div>
     );
 }
