9097daa1ffac3f52189ab3075f6bb25de9f2b95d
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-05-01 18:02:49 +0300

    Updates

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,39 @@
+import * as GoIcons from "react-icons/go";
+import * as FaIcons from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
         <div className="footer">
-            <h2>Footer</h2>
+            <div className="input-elements">
+                <h2>Stay Informed Our Updates</h2>
+                <div className="input-icon">
+                    <input type="text" placeholder="Enter your email" />
+                    <GoIcons.GoMail id="input-email" />
+                </div>
+            </div>
+
+            <nav className="footer-nav">
+                <ul>
+                    <li>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/ivansolenkov99/"><FaIcons.FaInstagram /></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/ivan-solenkov-7544211ba/"><FaIcons.FaLinkedin /></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/IvanSolenkov"><FaIcons.FaGithub /></a>
+                    </li>
+                </ul>
+            </nav>
+
+            <p>© 2021 All rights reserved. Auto Insurance!</p>
         </div>
     );
 }
