46a5c9e6f71e4561c69e1317cb0ff5686b600ca5
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-05-07 14:26:21 +0300

    Updates

--- src/Components/Tour/Tour.jsx
+++ src/Components/Tour/Tour.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Tour = () => {
+    const [message, setMessage] = useState('');
+
+    const tourButton = (e) => {
+        e.preventDefault();
+
+        let name = document.querySelector('#name-tour');
+        let mobile = document.querySelector('#mobile-tour');
+        let mail = document.querySelector('#mail-tour');
+        let city = document.querySelector('#city-tour');
+
+        let msg = document.querySelector('.tour-msg');
+
+        if (name.value !== '' && mobile.value !== '' && mail.value !== '' && city.value !== '') {
+            setMessage('Your car is ready!');
+            msg.style.color = '#08bd08';
+        } else {
+            setMessage('Please fill in required fields!');
+            msg.style.color = 'red';
+        }
+    }
     return (
         <div className="tour">
             <div className="tour-text">
@@ -18,13 +39,14 @@ const Tour = () => {
                     Sed ut perspiciatis unde omnis iste natus earchitecto betae vitaee.
                 </p>
                 <form action="/" className="tour-form">
-                    <input type="text" placeholder="Your Name*" />
-                    <input type="number" placeholder="Mobile*" />
-                    <input type="email" placeholder="E-mail*" />
-                    <input type="text" placeholder="City*" />
+                    <input type="text" placeholder="Your Name*" id="name-tour" />
+                    <input type="number" placeholder="Mobile*" id="mobile-tour" />
+                    <input type="email" placeholder="E-mail*" id="mail-tour" />
+                    <input type="text" placeholder="City*" id="city-tour" />
                     <input type="text" placeholder="Zip Code" />
-                    <button>Get Quote</button>
+                    <button onClick={tourButton}>Get Quote</button>
                 </form>
+                <span className="tour-msg">{message}</span>
             </div>
 
         </div>
